62cc548797bb68e78032b694772dfe3d3ded9597
Abhishek Ranjan <rabhishekranjan@gmail.com>
2022-02-13 04:37:45 +0530

    implemented the function buyTokens() and withdraw()

--- packages/hardhat/contracts/Vendor.sol
+++ packages/hardhat/contracts/Vendor.sol
@@ -9,10 +9,32 @@ contract Vendor is Ownable {
   //event BuyTokens(address buyer, uint256 amountOfETH, uint256 amountOfTokens);
 
   YourToken public yourToken;
+  uint256 constant public tokensPerETH = 100;
+  event BuyTokens(address buyer, uint amountOfETH, uint amountOfTokens);
 
   constructor(address tokenAddress) {
     yourToken = YourToken(tokenAddress);
   }
+
+  function buyTokens() payable public {
+    uint256 amountOfETH = msg.value;
+    uint256 amountOfTokens = amountOfETH * tokensPerETH;
+    yourToken.transfer(msg.sender, amountOfTokens);
+    emit BuyTokens(msg.sender, amountOfETH, amountOfTokens);
+
+  }
+
+  function withdraw() payable public {
+    address payable owner = payable(owner());
+    require(msg.sender == owner);
+    owner.transfer(address(this).balance);
+    //yourToken.transfer(, yourToken.balanceOf(msg.sender));
+  }
+     // yourToken.transfer(owner(), address(this).balance);
+
+  
+
+
 
   // ToDo: create a payable buyTokens() function:
 
--- packages/hardhat/contracts/YourToken.sol
+++ packages/hardhat/contracts/YourToken.sol
@@ -6,6 +6,10 @@ import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
 contract YourToken is ERC20 {
     constructor() ERC20("Gold", "GLD") {
-        _mint( msg.sender, 1000 * 10 ** 18);
+        _mint( msg.sender , 1000 * 10 ** 18);
     }
 }
+
+
+
+
